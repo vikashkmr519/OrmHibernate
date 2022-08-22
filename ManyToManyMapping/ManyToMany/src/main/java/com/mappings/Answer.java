e424f710b17408a16fb3fdc6bfc4a647d478cec6
vikashkmr519 <vikashkmr519@gmail.com>
2022-08-22 23:49:25 +0530

    ManyToMany

---
+++ ManyToManyMapping/ManyToMany/src/main/java/com/mappings/Answer.java
@@ -0,0 +1,71 @@
+package com.mappings;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Answer {
+	
+	@Id
+	@Column(name="answer_id")
+	private int answerId;
+	
+	private String answer;
+	
+	@ManyToOne
+	private Question question;
+
+
+	public Answer() {
+		super();
+	}
+
+
+	public Answer(int answerId, String answer, Question question) {
+		super();
+		this.answerId = answerId;
+		this.answer = answer;
+		this.question = question;
+	}
+
+
+	public int getAnswerId() {
+		return answerId;
+	}
+
+
+	public void setAnswerId(int answerId) {
+		this.answerId = answerId;
+	}
+
+
+	public String getAnswer() {
+		return answer;
+	}
+
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+
+	public Question getQuestion() {
+		return question;
+	}
+
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	
+
+	
+	
+	
+	
+
+}
